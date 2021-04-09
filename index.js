189505905ba9d46b5221c758e624e0ee1083a3ed
Matthew Vanni <mvanni4994@gmail.com>
2021-04-09 01:45:10 -0400

    added prompt inputs

--- index.js
+++ index.js
@@ -1,13 +1,141 @@
 const inquirer = require("inquirer");
-const fs = require ('fs')
+const fs = require ('fs');
 
+let newTeamProfile = []
+
+function createApp(){
+    teamPrompt()
+};
+
+var intern = require('./StaffLib/intern');
+var manager = require('./StaffLib/manager');
+var engineer = require('./StaffLib/engineer');
+var employee = require('./StaffLib/employ');
+
+function managerPrompt(){
 inquirer.prompt([
     {
-        name: "Greeting",
-        message:"What would you like to say?",
-        type:"input"
-    }
-])
-.then(function(answer){
-    console.log(answer);
-});
+        type:"input",
+        name: "Name",
+        message:"What is the Manager's name?"
+    },
+    {
+        type:"input",
+        name: "id",
+        message:"What is the Manager's employee ID?"
+    },
+    {
+        type:"input",
+        name: "email",
+        message:"What is the Manager's email address?"
+    },
+    {
+        type:"input",
+        name:"officeNumber",
+        message:"What is the office number of the manager?"
+    },
+]).then(function(response) {
+    var name = response.name;
+    var id = response.id;
+    var email = response.email;
+    var officeNumber = response.officeNumber;
+    const newMem = new Manager(name, id, email, officeNumber);
+    newTeamProfile.push(newMem);
+    teamPrompt();
+})
+};
+
+function internPrompt(){
+    inquirer.prompt([
+        {
+            type:"input",
+            name: "Name",
+            message:"What is the intern's name?"
+        },
+        {
+            type:"input",
+            name: "id",
+            message:"What is the intern's employee ID?"
+        },
+        {
+            type:"input",
+            name: "email",
+            message:"What is the intern's email address?"
+        },
+        {
+            type:"input",
+            name:"internSchool",
+            message:"What school did the intern attend?"
+        },
+    ]).then(function(response) {
+        var name = response.name;
+        var id = response.id;
+        var email = response.email;
+        var internSchool = response.internSchool;
+        const newMem = new Intern(name, id, email, internSchool);
+        newTeamProfile.push(newMem);
+        teamPrompt();
+    })
+    };
+
+    function engineerPrompt(){
+        inquirer.prompt([
+            {
+                type:"input",
+                name: "Name",
+                message:"What is the engineer's name?"
+            },
+            {
+                type:"input",
+                name: "id",
+                message:"What is the engineer's employee ID?"
+            },
+            {
+                type:"input",
+                name: "email",
+                message:"What is the engineer's email address?"
+            },
+            {
+                type:"input",
+                name:"engineerGithub",
+                message:"What is the engineer's Github username?"
+            },
+        ]).then(function(response) {
+            var name = response.name;
+            var id = response.id;
+            var email = response.email;
+            var engineerGithub = response.engineerGithub;
+            const newMem = new Engineer(name, id, email, engineerGithub);
+            newTeamProfile.push(newMem);
+            teamPrompt();
+        })
+        };
+
+        function employeePrompt(){
+            inquirer.prompt([
+                {
+                    type:"input",
+                    name: "Name",
+                    message:"What is the employee's name?"
+                },
+                {
+                    type:"input",
+                    name: "id",
+                    message:"What is the employee's employee ID?"
+                },
+                {
+                    type:"input",
+                    name: "email",
+                    message:"What is the employee's email address?"
+                },
+            ]).then(function(response) {
+                var name = response.name;
+                var id = response.id;
+                var email = response.email;
+                const newMem = new Manager(name, id, email);
+                newTeamProfile.push(newMem);
+                teamPrompt();
+            })
+            };
+
+            
