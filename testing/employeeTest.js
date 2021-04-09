51a5572aa13de170b0f96edb2f87e65d6d518adf
Matthew Vanni <mvanni4994@gmail.com>
2021-04-09 02:37:47 -0400

    added engineer test

--- testing/employeeTest.js
+++ testing/employeeTest.js
@@ -2,7 +2,7 @@ const Employee = require ("../StaffLib/employee");
 
 describe("getRole",() => {
     it("should return 'Employee' from getRole()",()=> {
-        const employee = new Employee("Noctis", 27, "noctis@insomnia.com", "Noctis Caelum");
+        const employee = new Employee("Noctis", 27, "noctis@insomnia.com");
         console.log(employee);
         expect(employee.getRole()).to.equal("Employee");
     });
@@ -10,7 +10,7 @@ describe("getRole",() => {
 
 describe("getName", () => {
     it("should return 'Noctis' from getName()",()=> {
-        const employee = new Employee("Noctis", 27, "noctis@insomnia.com", "Noctis Caelum");
+        const employee = new Employee("Noctis", 27, "noctis@insomnia.com");
         console.log(employee);
         expect(employee.getName()).to.equal("Noctis");
     });
@@ -18,14 +18,14 @@ describe("getName", () => {
 
 describe("getID", () => {
     it("should return '15' from getID()",()=> {
-        const employee = new Employee("Noctis", 27, "noctis@insomnia.com", "Noctis Caelum");
+        const employee = new Employee("Noctis", 27, "noctis@insomnia.com");
         console.log(employee);
-        expect(employee.getID()).to.equal("15");
+        expect(employee.getID()).to.equal("27");
     });
 });
 describe("getEmail", () => {
-    it("should return 'noctis@insomnia.com' from getID()",()=> {
-        const employee = new Employee("Noctis", 27, "noctis@insomnia.com", "Noctis Caelum");
+    it("should return 'noctis@insomnia.com' from getEmail()",()=> {
+        const employee = new Employee("Noctis", 27, "noctis@insomnia.com");
         console.log(employee);
         expect(employee.getEmail()).to.equal("noctis@insomnia.com");
     });
--- testing/engineerTest.js
+++ testing/engineerTest.js
@@ -1,1 +1,41 @@
-cont engineer = require()
+const Engineer = require("../StaffLib/engineer");
+
+describe("getRole",() => {
+    it("should return 'Engineer' from getRole()",()=> {
+        const engineer = new Engineer("Lunafreya", 26, "Lunafreya@insomnia.com", "LunafreyaFleurent");
+        console.log(engineer);
+        expect(engineer.getRole()).to.equal("engineer");
+    });
+});
+
+describe("getName", () => {
+    it("should return 'Lunafreya' from getName()",()=> {
+        const engineer = new Engineer("Lunafreya", 26, "Lunafreya@insomnia.com", "LunafreyaFleurent");
+        console.log(engineer);
+        expect(engineer.getName()).to.equal("Lunafreya");
+    });
+});
+
+describe("getID", () => {
+    it("should return '15' from getID()",()=> {
+        const engineer = new Engineer("Lunafreya", 26, "Lunafreya@insomnia.com", "LunafreyaFleurent");
+        console.log(engineer);
+        expect(engineer.getID()).to.equal("26");
+    });
+});
+
+describe("getEmail", () => {
+    it("should return 'noctis@insomnia.com' from getEmail()",()=> {
+        const engineer = new Engineer("Lunafreya", 26, "Lunafreya@insomnia.com", "LunafreyaFleurent");
+        console.log(engineer);
+        expect(engineer.getEmail()).to.equal("Lunafreya@insomnia.com");
+    });
+});
+
+describe("getGithub", () => {
+    it("should return 'Lunafreya' from getGithub()",()=> {
+        const engineer = new Engineer("Lunafreya", 26, "Lunafreya@insomnia.com", "LunafreyaFleurent");
+        console.log(engineer);
+        expect(engineer.getGithub()).to.equal("LunafreyaFleurent");
+    });
+});
