4156d73fe798f3392cdfdf90064bd6b3e3712ec0
Matthew Vanni <mvanni4994@gmail.com>
2021-04-10 00:20:02 -0400

    connected team prompts properly

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-const inquirer = require("inquirer");
-const fs = require ('fs');
+const inquirer = require('inquirer')
+const fs = require('fs')
 
 let newTeamProfile = []
 
@@ -7,265 +7,253 @@ function createApp(){
     teamPrompt()
 }
 
-var Intern = require('./StaffLib/intern');
-var Manager = require('./StaffLib/manager');
-var Engineer = require('./StaffLib/engineer');
-var Employee = require('./StaffLib/employee');
+var Manager = require("./staffLib/manager.js")
+var Engineer = require("./staffLib/engineer.js")
+var Intern = require("./staffLib/intern.js")
+
 
 function managerPrompt(){
-inquirer.prompt([
-    {
-        type:"input",
-        name: "Name",
-        message:"What is the Manager's name?"
-    },
-    {
-        type:"input",
-        name: "id",
-        message:"What is the Manager's employee id?"
-    },
-    {
-        type:"input",
-        name: "email",
-        message:"What is the Manager's email address?"
-    },
-    {
-        type:"input",
-        name:"officeNumber",
-        message:"What is the office number of the manager?"
-    },
-]).then(function(response) {
-    var name = response.name;
-    var id = response.id;
-    var email = response.email;
-    var officeNumber = response.officeNumber;
-    const newMem = new Manager(name, id, email, officeNumber);
-    newTeamProfile.push(newMem);
-    teamPrompt();
-})
-}
-
-function internPrompt(){
     inquirer.prompt([
         {
             type:"input",
             name: "Name",
-            message:"What is the intern's name?"
+            message:"What is the Manager's name?"
         },
         {
             type:"input",
             name: "id",
-            message:"What is the intern's employee id?"
+            message:"What is the Manager's employee id?"
         },
         {
             type:"input",
             name: "email",
-            message:"What is the intern's email address?"
+            message:"What is the Manager's email address?"
         },
         {
             type:"input",
-            name:"internSchool",
-            message:"What school did the intern attend?"
+            name:"officeNumber",
+            message:"What is the office number of the manager?"
         },
     ]).then(function(response) {
         var name = response.name;
         var id = response.id;
         var email = response.email;
-        var internSchool = response.internSchool;
-        const newMem = new Intern(name, id, email, internSchool);
+        var officeNumber = response.officeNumber;
+        const newMem = new Manager(name, id, email, officeNumber);
         newTeamProfile.push(newMem);
         teamPrompt();
     })
     }
-
-    function engineerPrompt(){
+    
+    function internPrompt(){
         inquirer.prompt([
             {
                 type:"input",
                 name: "Name",
-                message:"What is the engineer's name?"
+                message:"What is the intern's name?"
             },
             {
                 type:"input",
                 name: "id",
-                message:"What is the engineer's employee id?"
+                message:"What is the intern's employee id?"
             },
             {
                 type:"input",
                 name: "email",
-                message:"What is the engineer's email address?"
+                message:"What is the intern's email address?"
             },
             {
                 type:"input",
-                name:"engineerGithub",
-                message:"What is the engineer's Github username?"
+                name:"internSchool",
+                message:"What school did the intern attend?"
             },
         ]).then(function(response) {
             var name = response.name;
             var id = response.id;
             var email = response.email;
-            var engineerGithub = response.engineerGithub;
-            const newMem = new Engineer(name, id, email, engineerGithub);
+            var internSchool = response.internSchool;
+            const newMem = new Intern(name, id, email, internSchool);
             newTeamProfile.push(newMem);
             teamPrompt();
         })
         }
-
-        function employeePrompt(){
+    
+        function engineerPrompt(){
             inquirer.prompt([
                 {
                     type:"input",
                     name: "Name",
-                    message:"What is the employee's name?"
+                    message:"What is the engineer's name?"
                 },
                 {
                     type:"input",
                     name: "id",
-                    message:"What is the employee's employee id?"
+                    message:"What is the engineer's employee id?"
                 },
                 {
                     type:"input",
                     name: "email",
-                    message:"What is the employee's email address?"
+                    message:"What is the engineer's email address?"
+                },
+                {
+                    type:"input",
+                    name:"engineerGithub",
+                    message:"What is the engineer's Github username?"
                 },
             ]).then(function(response) {
                 var name = response.name;
                 var id = response.id;
                 var email = response.email;
-                const newMem = new Manager(name, id, email);
+                var engineerGithub = response.engineerGithub;
+                const newMem = new Engineer(name, id, email, engineerGithub);
                 newTeamProfile.push(newMem);
                 teamPrompt();
             })
             }
-
-            function teamPrompt() {
+    
+            function employeePrompt(){
                 inquirer.prompt([
                     {
-                        type: 'list',
-                        name: 'addMore',
-                        message: 'Would you like to add an employee?',
-                        choices: ['Engineer.', 'Intern.', 'Manager.','Employee.', 'No.']
+                        type:"input",
+                        name: "Name",
+                        message:"What is the employee's name?"
                     },
-                ])
-            .then(function(response){
-                    switch(response.addMore){
-                        case "Add an Engineer.":
-                            engineerPrompt();
-                            break;
-        
-                        case "Add an Intern.":
-                            internPrompt();
-                            break;
-        
-                        case "Add a Manager.":
-                            managerPrompt();
-                            break;
-        
-                        case "Add an Employee.":
-                            employeePrompt();
-                            break;    
-
-                        case "No.":
-                            completeTeam();
-                            break;
-                    }
-                });
-            }
-
-            function completeTeam(){
-                console.log("Success!")
-
-                beginHTML();
-                inputHTML();
-                endHTML();
-            }
-
-            function beginHTML(){
-                const data = `
-                <!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
-    <title>My Team</title>
-</head>
-<body>
-
-    <nav>
-        <div class="nav-wrapper">
-          <a class="brand-logo center">My Team</a>
-          <ul id="nav-mobile" class="left hide-on-med-and-down">
-          </ul>
-        </div>
-      </nav>
-
-      <div class="row">`
-
-      fs.writeFile('index.html', data, (error) => {
-          error ? console.error(error) : console.log('Success!')
-      })
-    }
-            function inputHTML(){
-                let data = ""
-                newTeamProfile.forEach(function(employee){
-                    if (employee.getRole() === "Manager"){
-                        data +=
-                        `<div class="card" style="width: 18rem;">
-                        <div class="card-header" id="topBar">Manager</div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item" id="name">${employee.name}</li>
-                                <li class="list-group-item">id: ${employee.id}</li>
-                                <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
-                                <li class="list-group-item">Office Number: ${employee.officeNumber}</li>
-                            </ul>
-                        </div>`
-                    } else if(employee.getRole() ==="Engineer"){
-                        data +=
-                        `<div class="card" style="width: 18rem;">
-                        <div class="card-header" id="topBar">Engineer</div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item" id="name">${employee.name}</li>
-                                <li class="list-group-item">id: ${employee.id}</li>
-                                <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
-                                <li class="list-group-item">Office Number: ${employee.engineerGithub}</li>
-                            </ul>
-                        </div>`
-            } else if (employee.getRole() ==="Intern"){
-                    data +=
-                    `<div class="card" style="width: 18rem;">
-                    <div class="card-header" id="topBar">Intern</div>
-                        <ul class="list-group list-group-flush">
-                            <li class="list-group-item" id="name">${employee.name}</li>
-                            <li class="list-group-item">id: ${employee.id}</li>
-                            <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
-                            <li class="list-group-item">Office Number: ${employee.internSchool}</li>
-                        </ul>
-                    </div>`
-                } else{
-                        data +=
-                        `<div class="card" style="width: 18rem;">
-                        <div class="card-header" id="topBar">Engineer</div>
-                            <ul class="list-group list-group-flush">
-                                <li class="list-group-item" id="name">${employee.name}</li>
-                                <li class="list-group-item">id: ${employee.id}</li>
-                                <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
-                            </ul>
-                        </div>`
-                    }
-                })
-        fs.appendFile ("index.html", data, function (error){
-            error? console.error(error): console.log('Success!')
-        })
+                    {
+                        type:"input",
+                        name: "id",
+                        message:"What is the employee's employee id?"
+                    },
+                    {
+                        type:"input",
+                        name: "email",
+                        message:"What is the employee's email address?"
+                    },
+    ]).then(function(response){
+        var name = response.name;
+        var ID = response.ID;
+        var email = response.email;
+        var internSchool = response.internSchool
+        const newTeamMem = new Intern(name, ID, email, internSchool);
+        newTeamProfile.push(newTeamMem);
+        teamPrompt();
+    })
+} 
     
-    }
+    function teamPrompt(){
+        inquirer.prompt([
+            {
+                type: 'list',
+                name: 'addMore',
+                message: 'Who do you want to add?',
+                choices: ['Engineer.', 'Intern.', 'Manager.', 'Complete.']
+            },
+        ])
+        .then(function (response) {
 
-    function endHTML(){
-        data = `</div>
-        </body>
-        </html>`
-        fs.appendFile ("index.html", data, function (error){
-            error ? console.error(error): console.log('Success!')
+            switch (response.addMore) {
+                case "Engineer.":
+                    engineerPrompt();
+                    break;
+
+                case "Intern.":
+                    internPrompt();
+                    break;
+
+                case "Manager.":
+                    managerPrompt();
+                    break;
+
+                case "Complete.":
+                    completeTeam();
+                    break;
+            }
         });
     }
-    createApp()
+
+function completeTeam() {
+    console.log("Success! Team has been created.")
+    
+    beginHTML()
+    inputHTML();
+    endHTML();
+}
+    
+function beginHTML(){
+    const data = `
+    <!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.5.3/dist/css/bootstrap.min.css" integrity="sha384-TX8t27EcRE3e/ihU7zmQxVncDAy5uIKz4rEkgIXeMed4M0jlfIDPvg6uqKI2xXr2" crossorigin="anonymous">
+        <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/3.2.1/jquery.min.js"></script>
+        <link rel="stylesheet" href="./dist/style.css">
+        <title>My Team</title>
+        </head>
+        <body>
+            <nav class="navbar navbar-dark bg-dark">
+                <span class="navbar-brand mb-0 h1 w-100 text-center">My Team</span>
+                <p id="myTeam" class="lead"></p>
+            </nav>
+            <div class="row">
+            `
+    fs.writeFile('index.html', data, (error) => {
+        error ? console.error(error) : console.log('Success!')
+});
+}
+
+function inputHTML(){
+    let data = ""
+    newTeamProfile.forEach(function(employee){
+        if (employee.getRole() === "Manager"){
+            data +=
+            `<div class="card" style="width: 18rem;">
+            <div class="card-header" id="topBar">Manager</div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item" id="name">${employee.name}</li>
+                    <li class="list-group-item">ID: ${employee.ID}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
+                    <li class="list-group-item">Office Number: ${employee.managerNumber}</li>
+                </ul>
+            </div>`
+        } else if (employee.getRole() === "Engineer"){
+            data +=
+            `<div class="card" style="width: 18rem;">
+            <div class="card-header" id="topBar">Engineer</div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item" id="name">${employee.name}</li>
+                    <li class="list-group-item">ID: ${employee.ID}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
+                    <li class="list-group-item"><a href="https://github.com/${employee.engineerGithub}">https://github.com/${employee.engineerGithub}</a></li>
+                </ul>
+            </div>`
+        } else {
+            data +=
+            `<div class="card" style="width: 18rem;">
+            <div class="card-header" id="topBar">Intern</div>
+                <ul class="list-group list-group-flush">
+                    <li class="list-group-item" id="name">${employee.name}</li>
+                    <li class="list-group-item">ID: ${employee.ID}</li>
+                    <li class="list-group-item">Email: <a href="mailto:${employee.email}">${employee.email}</a></li>
+                    <li class="list-group-item">School: ${employee.internSchool}</li>
+                </ul>
+            </div>`
+        }
+    })
+        
+        fs.appendFile("index.html", data, function (error) {
+            error ? console.error(error) : console.log('success!')
+        })
+}
+
+function endHTML(){
+    data = 
+    `</div>
+    </body>
+    </html>
+        `
+    fs.appendFile("index.html", data, function (error) {
+        error ? console.error(error) : console.log('success!')
+    });
+}
+createApp();
